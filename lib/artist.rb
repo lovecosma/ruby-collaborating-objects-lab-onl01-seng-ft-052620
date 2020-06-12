36a8f2629c065e83400c171fc4528b465d117bfa
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-12 17:11:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,7 +18,12 @@ self.songs << song
 end
 
 def self.find_or_create_by_name(artist_name)
+if @@all.any?
+  @@all.each do |artist|
+    return artist if artist.name == artist_name
+  end
 
+end 
 end
 
 
